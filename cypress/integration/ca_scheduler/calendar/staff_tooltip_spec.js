7a987f9cbea57f6612cfc6d1cf8045f1afaad696
Alex Landis <alandis@daxkocom>
2019-09-24 14:57:32 -0500

    beta updated staff tooltip spec

--- cypress/integration/ca_scheduler/calendar/staff_tooltip_spec.js
+++ cypress/integration/ca_scheduler/calendar/staff_tooltip_spec.js
@@ -1,5 +1,7 @@
 describe('Loads calendar tooltip for staff', () => {
     let staffName
+    let staffId
+    let serviceTypes
     context('through UI', () => {
         beforeEach(() => {
             cy.server()
@@ -16,16 +18,28 @@ describe('Loads calendar tooltip for staff', () => {
                 '/api/schedule/staff/shifts**'
             ).as('getStaffShift')
             cy.visit('/scheduler')
-            cy.wait('@getStaff').then((xhr) => {
-                if(xhr.status === 200) {
-                    staffName = xhr.responseBody.data[0].name
-                }
-            })
+            cy.wait('@getStaff')
         })
         it('displays staff name and calendar options', () => {
             cy.wait('@getEvents')
-            cy.get(':nth-child(2) > .cac-col-header > h4 > .title').scrollIntoView().click()
-            cy.wait('@getStaffShift')
+            cy.get('.cac-cols > .cac-column:nth-child(1) > .cac-col-header > h4 > .title').click({force:true})
+            cy.get('.staff-detail-tooltip').should('be.visible')
+            cy.wait('@getStaffShift').then((xhr) => {
+                if(xhr.status ===200) {
+                    staffId = xhr.responseBody.data[0].user.id
+                    staffName = xhr.responseBody.data[0].user.name
+
+                    serviceTypes = xhr.responseBody.data[0].serviceTypes
+
+                    cy.get('.staff-detail-tooltip').find('h3').contains(staffName).should('have.attr', 'href', '/user/edit?id=' + staffId)
+                    cy.get('.staff-detail-tooltip').find('li').each(($li) => {
+                    cy.log($li)
+                    })
+                    //.then(($li) => {
+                        //expect($li).to.have.length(5)
+                    //})
+                }
+            })
         })
     })
 })
