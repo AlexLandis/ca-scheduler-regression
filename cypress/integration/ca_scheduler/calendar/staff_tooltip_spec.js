47530687ed3ffa5672e6c40c7f4a590f469939eb
Alex Landis <alandis@daxkocom>
2019-09-25 11:43:39 -0500

    beta separated tests in tooltip

--- cypress/integration/ca_scheduler/calendar/staff_tooltip_spec.js
+++ cypress/integration/ca_scheduler/calendar/staff_tooltip_spec.js
@@ -20,7 +20,7 @@ describe('Loads calendar tooltip for staff', () => {
             cy.visit('/scheduler')
             cy.wait('@getStaff')
         })
-        it('displays staff name and calendar options', () => {
+        it('displays staff name that links to staff profile', () => {
             cy.wait('@getEvents')
             cy.get('.cac-cols > .cac-column:nth-child(1) > .cac-col-header > h4 > .title').click({force:true})
             cy.get('.staff-detail-tooltip').should('be.visible')
@@ -32,9 +32,28 @@ describe('Loads calendar tooltip for staff', () => {
                     serviceTypes = xhr.responseBody.data[0].serviceTypes
 
                     cy.get('.staff-detail-tooltip').find('h3').contains(staffName).should('have.attr', 'href', '/user/edit?id=' + staffId)
-                    cy.get('.staff-detail-tooltip').find('li').each(($li) => {
-                    cy.log($li)
-                    })
+                    
+            
+                    //cy.get('.staff-detail-tooltip').find('li').each(($li) => {
+                    //cy.log($li)
+                    //})
+                    //.then(($li) => {
+                        //expect($li).to.have.length(5)
+                    //})
+                }
+            })
+        })
+
+        it('displays available service types for staff member', () => {
+            cy.wait('@getEvents')
+            cy.get('.cac-cols > .cac-column:nth-child(1) > .cac-col-header > h4 > .title').click({force:true})
+
+            cy.wait('@getStaffShift').then((xhr) => {
+                if(xhr.status ===200) {
+                    serviceTypes = xhr.responseBody.data[0].serviceTypes
+                    //cy.get('.staff-detail-tooltip').find('li').each(($li) => {
+                    //cy.log($li)
+                    //})
                     //.then(($li) => {
                         //expect($li).to.have.length(5)
                     //})
