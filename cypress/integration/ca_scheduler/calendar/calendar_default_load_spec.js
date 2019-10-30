38641f733fb6f94d0d958ad179a52d6280bec756
Alex Landis <alandis@daxkocom>
2019-10-30 13:25:44 -0500

    beta fixed issues with load calendar

--- cypress/integration/ca_scheduler/calendar/calendar_default_load_spec.js
+++ cypress/integration/ca_scheduler/calendar/calendar_default_load_spec.js
@@ -1,11 +1,11 @@
 describe('Loads the calendar page', () => {
     context('through UI', () => {
-        it('should load default calendar view', () => {
+        it('should load default calendar view - Staff/Today', () => {
             cy.visit('/scheduler')
             cy.get('.filter-panel-body').should('be.visible')
             cy.get('.ca-calendar ').should('be.visible')
-            cy.get('.component-name').should('be.visible')
-            cy.get('.ca-ui-toggle-group ').children().as('range').should('be.visible').and('have.length', 4)
+            cy.get('.title-primary').should('be.visible')
+            cy.get('.ca-ui-toggle-group ').children().as('range').should('be.visible')
             cy.get('@range').first().should('contain', 'Today').and('have.css', 'background-color', 'rgb(19, 151, 225)')
             cy.get('@range').eq(1).should('contain', 'Week').and('not.have.css', 'background-color', 'rgb(19, 151, 225)')
             cy.get('.basis-toggle-btn').contains('By Staff').should('have.css', 'border-bottom-color', 'rgb(19, 151, 225)').and('have.class', 'active')
